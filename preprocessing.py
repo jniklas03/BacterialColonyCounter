2cc90186ef288c4b0cfa40123147d77cf76a8f49
jniklas03 <jakubniklas@gmail.com>
2025-09-22 13:37:15 +0200

    expanded the theshholding code to where it performs reasonably well on the incubator image with multiple dishes

--- preprocessing.py
+++ preprocessing.py
@@ -1,32 +1,32 @@
 import cv2 as cv
 import numpy as np
 
-# first image - rpi; otsus binarisation
-img1 = cv.imread('images/newest_image_crop_unguenstig.jpg', cv.IMREAD_GRAYSCALE)
-cv.imwrite("gray1.png", img1)
-blur1 = cv.GaussianBlur(img1, (5,5), 0)
-ret1, th1 = cv.threshold(blur1,255,255,cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
-cv.imwrite("otsu1.png", th1)
+img = cv.imread("Images/newest_image.jpg", cv.IMREAD_GRAYSCALE)
+blur = cv.GaussianBlur(img, (101, 101), 0)
+clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
 
-#second image - colony counter; otsus binarisation
-img2 = cv.imread('images/WT1_P1_counts193_img1.png', cv.IMREAD_GRAYSCALE)
-cv.imwrite("gray2.png", img2)
-ret2, th2 = cv.threshold(img2,0,255,cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
-cv.imwrite("otsu2.png", th2)
+# corrected = cv.subtract(img, blur)
+# corrected = cv.normalize(corrected, None, 0, 255, cv.NORM_MINMAX)
 
-# noise removal
-kernel = np.ones((3,3), np.uint8)
-opening = cv.morphologyEx(th2,cv.MORPH_OPEN, kernel, iterations = 2)
+clahe1 = clahe.apply(img)
 
-# sure background area
-sure_bg = cv.dilate(opening,kernel,iterations=3)
-cv.imwrite("surebg.png", sure_bg)
+kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (211, 211)) #around 211-221
+background = cv.morphologyEx(img, cv.MORPH_OPEN, kernel)
+corrected = cv.subtract(img, background)
 
- 
-# Finding sure foreground area
-dist_transform = cv.distanceTransform(opening,cv.DIST_L2,5)
-ret, sure_fg = cv.threshold(dist_transform,0.7*dist_transform.max(),255,0)
- 
-# Finding unknown region
-sure_fg = np.uint8(sure_fg)
-unknown = cv.subtract(sure_bg,sure_fg)
+clahe2 = clahe.apply(corrected)
+
+thresh = cv.adaptiveThreshold(
+    corrected, 
+    255, 
+    cv.ADAPTIVE_THRESH_GAUSSIAN_C,
+    cv.THRESH_BINARY,
+    51,
+    5)
+
+
+cv.imwrite("clahe1.jpg", clahe1)
+cv.imwrite("clahe2.jpg", clahe2)
+cv.imwrite("corrected.jpg", corrected)
+cv.imwrite("background.jpg", background)
+cv.imwrite("thresh.jpg", thresh)
