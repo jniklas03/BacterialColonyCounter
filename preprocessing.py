0215c5c2440b407ff553f69bf2d9813febb69fc2
jniklas03 <jakubniklas@gmail.com>
2025-09-24 14:04:43 +0200

    split up preprocessing, dish_detection, colony detection and reading the images into different files

--- preprocessing.py
+++ preprocessing.py
@@ -1,40 +1,47 @@
 import cv2 as cv
 import numpy as np
+import os
 
-img = cv.imread("SOURCE.jpg", cv.IMREAD_GRAYSCALE)
-blur = cv.GaussianBlur(img, (101, 101), 0)
-clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (221, 221)) #around 211-221
+def preprocess(
+        gray_img,
+        save_path,
+        save=False,
+        kernel_size=211,
+        ):
+  
+    clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (kernel_size, kernel_size)) #around 211-221
+    background = cv.morphologyEx(gray_img, cv.MORPH_OPEN, kernel)
+    corrected = cv.subtract(gray_img, background)
+    corrected = clahe.apply(corrected)
 
-# preprocessing var 2; background removal, then clahe
-background2 = cv.morphologyEx(img, cv.MORPH_OPEN, kernel)
-corrected2 = cv.subtract(img, background2)
-corrected2 = clahe.apply(corrected2)
+    if save:
+        save_path_preprocessing = save_path + r"\Preprocessing"
+        os.makedirs(save_path_preprocessing, exist_ok=True)
+        cv.imwrite(os.path.join(save_path_preprocessing, "preprocessed.jpg"), corrected)
+    
+    print("Preprocessing successful.")
 
-# cv.imwrite("thresh2.jpg", thresh2)
-# cv.imwrite("corr2.jpg", corrected2)
+    return(corrected)
 
-params = cv.SimpleBlobDetector_Params()
-params.minThreshold = 0
-params.maxThreshold = 255
-params.thresholdStep = 10
-params.filterByArea = True
-params.minArea = 100
-params.maxArea = 1000
-params.filterByColor = True
-params.blobColor = 0
-params.filterByCircularity = True
-params.minCircularity = 0.3
-params.filterByConvexity = False
-# params.minConvexity = 0.9
-params.filterByInertia = True
-params.minInertiaRatio = 0.2
+def preprocess_dishes(
+        gray_img,
+        save_path,
+        save=False,
+        kernel_size=211,
+        ):
+  
+    clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (kernel_size, kernel_size)) #around 211-221
+    background = cv.morphologyEx(gray_img, cv.MORPH_OPEN, kernel)
+    corrected = cv.subtract(gray_img, background)
+    corrected = clahe.apply(corrected)
 
-detector = cv.SimpleBlobDetector_create(params)
-
-blobs = detector.detect(corrected2)
-
-output = cv.drawKeypoints(img, blobs, np.array([]), (0,0,255), cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-cv.imwrite("Output.jpg", output)
-print("CFU:", len(blobs))
+    if save:
+        save_path_preprocessing = save_path + r"\Preprocessing"
+        os.makedirs(save_path_preprocessing, exist_ok=True)
+        cv.imwrite(os.path.join(save_path_preprocessing, "preprocessed.jpg"), corrected)
+    
+    print("Preprocessing successful.")
+    
+    return(corrected)
