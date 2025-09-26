b5092d6d462fdce252eadb8209a5c768174b5efc
jniklas03 <jakubniklas@gmail.com>
2025-09-26 11:21:47 +0200

    refactored code into a function and changed how the data is saved

--- read_pdf.py
+++ read_pdf.py
@@ -1,20 +1,54 @@
 from pypdf import PdfReader
+import os
 import re
 
-pattern_count = re.compile(r"Count :\s*(\d+)")
-pattern_name = re.compile(r"Sample N°\s*:\s*.*?\bWt\s+(\d+)\s+Platte\s+(\d+)", re.DOTALL)
+def read_pdf(
+        source,
+        file,
+        save_path,
+        save_images=False
+):
+    save_path_extracted_pdf = save_path + r"\FromPDF"
+    os.makedirs(save_path_extracted_pdf, exist_ok=True)
 
-doc= PdfReader("tag0.pdf")
+    pattern_count = re.compile(r"Count :\s*(\d+)")
+    pattern_name = re.compile(r"(WT\s*\d+\s+P\s*\d+)")
 
-for page_idx, page in enumerate(doc.pages, start=0):
-    text = page.extract_text()
+    doc = PdfReader(source)
 
-    counts = pattern_count.findall(text)
+    all_counts = []
+    all_names = []
 
-    names = pattern_name.findall(text)
+    for page in doc.pages:
+        text = page.extract_text()
 
-    for image_idx, image_file_object in enumerate(page.images[1:], start=1):
+        count = int(pattern_count.findall(text)[0])
+        all_counts.append(count)
 
-        filename = f"WT{names[0][0]}_P{names[0][1]}_counts{counts[0]}_img{image_idx}.png"
-        with open(filename, "wb") as fp:
-            fp.write(image_file_object.data)
+        name = str(pattern_name.findall(text)[0]).replace(" ", "_")
+        all_names.append(name)
+
+        if save_images:
+            for image_idx, image_file_object in enumerate(page.images[1:2], start=1): #change the slice, and naming to get all images
+                filename = f"{file}_{name}"
+                with open(f"{os.path.join(save_path_extracted_pdf, filename)}.jpg", "wb") as fp:
+                    fp.write(image_file_object.data)
+
+
+    output = dict(zip(all_names, all_counts))
+
+    with open(f"{os.path.join(save_path_extracted_pdf, file)}.txt", "w") as f:
+        f.write(str(output))
+
+    return(output)
+
+
+
+
+
+read_pdf(
+    source=r"Results\PDFs\22.9.2025.pdf",
+    file="22.9.2025.pdf",
+    save_path=r"Results",
+    save_images=True
+)
