cd756ebc6276c35ae570f841031010233b62b670
jniklas03 <jakubniklas@gmail.com>
2025-10-01 17:00:36 +0200

    Added more docs and changed parameters to tuned ones

--- colony_detection.py
+++ colony_detection.py
@@ -3,61 +3,72 @@ import numpy as np
 import os
 
 def detect_colonies(
-        data,
         file,
         preprocessed_img,
         raw_img,
         save_path,
         tag = 0,
-        save=True
+        save=True,
+        data=None
 ):
     """
     Detects individual colonies. 
 
     Keyword arguments:
-    data -- data.yaml metadata file.
     file -- Name of the file without the extension, so "22.09.2025" NOT "22.09.2025.jpg".
     preprocessed_img -- openCV object of preprocessed image (grayscale).
     raw_img -- openCV object of raw/initial image.
     save_path -- Filepath where the images should be saved. The script makes different folders for different images by default.
     tag -- Internal parameter passed by main.py for processing and naming multiple dishes. Don't change.
     save -- Save the preprocessed images?
+    data -- data.yaml metadata file.
     """
-    params = cv.SimpleBlobDetector_Params()
+    params = cv.SimpleBlobDetector_Params() # Values from hyperparameter tuning
+
     params.minThreshold = 0
-    params.maxThreshold = 255
-    params.thresholdStep = 10
-    params.filterByArea = True
-    params.minArea = 100
+    params.maxThreshold = 64 # Smaller values = less false positives
+    params.thresholdStep = 2.975050229896966 # Smaller values = more true positives
+
+    params.filterByArea = True # Area in pxs
+    params.minArea = 100 # Generous values
     params.maxArea = 1000
+
     params.filterByColor = True
-    params.blobColor = 0
+    params.blobColor = 0 # Accepts dark/black colonies after preprocessing
+
     params.filterByCircularity = True
-    params.minCircularity = 0.3
+    params.minCircularity = 0.41063466457046227
+
     params.filterByConvexity = False
-    # params.minConvexity = 0.9
+    params.minConvexity = 0.7208306409286382
+
     params.filterByInertia = True
-    params.minInertiaRatio = 0.2
+    params.minInertiaRatio = 0.11002280443338333
 
-    detector = cv.SimpleBlobDetector_create(params)
+    detector = cv.SimpleBlobDetector_create(params) # Creates detector object
+    blobs = detector.detect(preprocessed_img) # Blobs are markers around colonies
 
-    blobs = detector.detect(preprocessed_img)
+    output = cv.drawKeypoints(raw_img, blobs, np.array([]), (0,255,0), cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS) # Output = initial image with colonies marked
 
-    output = cv.drawKeypoints(raw_img, blobs, np.array([]), (0,255,0), cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-    if data:
+    if data: # If metadata file present (by running from main.py) saves colony count and optionally colony cooridnates
         dish = data[file]["dishes"][tag]
         dish["colony_count"] = len(blobs)
-        dish["colonies"] = [{"x": int(k.pt[0]), "y": int(k.pt[1]), "size": k.size} for k in blobs]
+        # dish["colonies"] = [{"x": int(blob.pt[0]), "y": int(blob.pt[1]), "size": round(blob.size, 3)} for blob in blobs]
 
 
-    if save:
+    if save: # Saving images with marked colonies
         save_path_blob_detection = save_path + r"\Colonies"
         os.makedirs(save_path_blob_detection, exist_ok=True)
         cv.imwrite(os.path.join(save_path_blob_detection, f"{file}_colonies_{tag+1}.jpg"), output)
-        # with open(f"{os.path.join(save_path_blob_detection, file)}.txt", "a") as fp:
-        #     fp.write(f"{len(blobs)} \n")
 
     print(f"{len(blobs)} colonies detected.")
 
     return(data)
+
+# detect_colonies(
+#     file=os.path.splitext(os.path.basename("Sources/23.09.2025.jpg"))[0],
+#     preprocessed_img=cv.imread("Preprocessing/23.09.2025_preprocessed_1.jpg"),
+#     raw_img=cv.imread("Dishes/23.09.2025_dish_1.png"),
+#     save_path=r"C:\Users\jakub\Documents\Bachelorarbeit\Code\160925",
+#     save=False
+# )
