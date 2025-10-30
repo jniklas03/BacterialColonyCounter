d373c24aef7c21c069904121c2bab41e6afafa9c
jniklas03 <jakubniklas03@gmail.com>
2025-10-30 13:37:40 +0100

    added the timelapse pipeline

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import cv2 as cv
 import numpy as np
 import os
 import yaml
-from dish_detection import detect_dishes
-from preprocessing import preprocess
+from inputs import read_time, read_image_paths
+from dish_detection import detect_dishes, crop
+from preprocessing import preprocess, preprocess_small
 from counting import detect_colonies
+from timelapse import *
 
 def pipeline(
         source,
@@ -112,3 +114,43 @@ def mult_pipeline(
             save_preprocessed=save_preprocessed,
             save_detected=save_detected
         )
+
+def timelapse_pipeline(
+        source,
+        save,
+        save_path = "",
+        n_to_stack=5
+):
+    image_paths, file_names = read_image_paths(source)
+
+    fg_masks, coordinates = make_foreground_masks(image_paths=image_paths, save_path=save_path, save=False)
+
+    bg_masks = make_background_masks(image_paths=image_paths, n_to_stack=n_to_stack, coordinates=coordinates, save_path=save_path, save=False)
+    
+    dish_counts = {i: [] for i in range(len(coordinates))}
+    
+    for img_path, file_name in zip(image_paths, file_names):
+        timestamp = read_time(img_path)
+
+        dishes, masks = crop(img_path, coordinates)
+        preprocessed_imgs = []
+
+        for idx, (dish, mask) in enumerate(zip(dishes, masks)):
+            preprocessed = preprocess_small(source=dish, mask=mask, file_name=file_name, save=False, save_path=save_path)
+
+            mask_applied = cv.bitwise_and(preprocessed, preprocessed, mask=fg_masks[idx])
+            if save:
+                cv.imwrite(os.path.join(save_path, f"{file_name}_mask1_dish{idx+1}.png"), mask_applied)
+
+            mask_applied = cv.bitwise_and(mask_applied, mask_applied, mask=cv.bitwise_not(bg_masks[idx]))
+            if save:
+                cv.imwrite(os.path.join(save_path, f"{file_name}_mask2_dish{idx+1}.png"), mask_applied)
+
+            preprocessed_imgs.append(mask_applied)
+
+        for idx, img in enumerate(preprocessed_imgs):
+            count, _, _ = detect_colonies(source=img, save=True, save_path=save_path, file_name=os.path.splitext(os.path.basename(img_path))[0], idx=idx+1)
+
+            dish_counts[idx].append((timestamp, count))
+
+    return(dish_counts)
