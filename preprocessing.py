f35ee349d2cfa5bd574df28d557924ad8eab531d
jniklas03 <jakubniklas03@gmail.com>
2025-11-04 11:38:06 +0100

    name changes and moving functions into other files

--- preprocessing.py
+++ preprocessing.py
@@ -8,70 +8,8 @@ from dish_detection import detect_dishes, crop
 
 def preprocess(
         source,
-        mask = None,
-        kernel_size = 200,
-        save=False,
-        save_path = "",
-        file_name = "preprocessed",
-        idx: int = None
-        ):
-    """
-    Preprocesses input image. Used for standalone colony detection or for foreground masking.
-    Returns preprocessed image.
-    
-    Parameters
-    ----------
-    source: str or np.ndarray
-        Image of dish, or string of to the image path
-    mask: np.ndarray, optional
-        Mask of background area outside of dish, passed by detect_dishes()
-    kernel_size: int, optional
-        Kernel size for "opening"; higher number yields more smoothed, generally better results, but takes longer. 
-    save: bool, default=True
-        Whether to save the cropped dishes.
-    save_path: str, optional
-        Path to directory where the image is saved.
-    file_name: str, optional
-        Name to save the preprocessed image as.
-    idx: int, optional
-        Passed by main.py if multiple dishes processed.
-
-    Returns
-    -------
-    np.ndarray
-        Preprocessed dish.
-    """
-    img = read_img(source=source)
-
-    if save and not save_path:
-        warnings.warn(f"No specified save path. Images saved in the current directory ({os.getcwd()}) under ...Preprocessing.")
-
-    green_channel = img[:, :, 1]
-
-    blur = cv.medianBlur(green_channel, 5)
-
-    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (kernel_size, kernel_size))
-
-    tophat = cv.morphologyEx(blur, cv.MORPH_TOPHAT, kernel)
-
-    _, threshold = cv.threshold(tophat, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
-
-    if mask is not None:
-        threshold = cv.bitwise_and(threshold, threshold, mask=mask)
-
-    save_name = f"{file_name}_p{idx}.png" if idx is not None else f"{file_name}_p.png"
-
-    if save:
-        save_path_preprocessing = os.path.join(save_path, "Preprocessing")
-        os.makedirs(save_path_preprocessing, exist_ok=True)
-        cv.imwrite(os.path.join(save_path_preprocessing, save_name), tophat)
-    print(f"File {save_name} preprocessed.")
-
-    return threshold
-
-def preprocess_small(
-        source,
         mask,
+        area_filter = True,
         s = 121,
         C = 11,
         kernel_size = 3,
@@ -128,13 +66,16 @@ def preprocess_small(
         C=C
     )
 
-    num_labels, labels, stats, centroids = cv.connectedComponentsWithStats(threshold, connectivity=8)
-    filtered = np.zeros_like(threshold)
+    if area_filter:
+        num_labels, labels, stats, centroids = cv.connectedComponentsWithStats(threshold, connectivity=8)
+        filtered = np.zeros_like(threshold)
 
-    for i in range(1, num_labels):  # skip background
-        area = stats[i, cv.CC_STAT_AREA]
-        if min_area <= area <= max_area:
-            filtered[labels == i] = 255
+        for i in range(1, num_labels):  # skip background
+            area = stats[i, cv.CC_STAT_AREA]
+            if min_area <= area <= max_area:
+                filtered[labels == i] = 255
+    else:
+        filtered = threshold
 
     kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (kernel_size, kernel_size))
     eroded = cv.morphologyEx(filtered, cv.MORPH_ERODE, kernel)
@@ -154,7 +95,70 @@ def preprocess_small(
 
     return filtered
 
-def preprocess_bg_removal(
+def preprocess_fg_isolation(
+        source,
+        mask = None,
+        kernel_size = 200,
+        save=False,
+        save_path = "",
+        file_name = "preprocessed",
+        idx: int = None
+        ):
+    """
+    Preprocesses input image. Used for standalone colony detection or for foreground masking.
+    Returns preprocessed image.
+    
+    Parameters
+    ----------
+    source: str or np.ndarray
+        Image of dish, or string of to the image path
+    mask: np.ndarray, optional
+        Mask of background area outside of dish, passed by detect_dishes()
+    kernel_size: int, optional
+        Kernel size for "opening"; higher number yields more smoothed, generally better results, but takes longer. 
+    save: bool, default=True
+        Whether to save the cropped dishes.
+    save_path: str, optional
+        Path to directory where the image is saved.
+    file_name: str, optional
+        Name to save the preprocessed image as.
+    idx: int, optional
+        Passed by main.py if multiple dishes processed.
+
+    Returns
+    -------
+    np.ndarray
+        Preprocessed dish.
+    """
+    img = read_img(source=source)
+
+    if save and not save_path:
+        warnings.warn(f"No specified save path. Images saved in the current directory ({os.getcwd()}) under ...Preprocessing.")
+
+    green_channel = img[:, :, 1]
+
+    blur = cv.medianBlur(green_channel, 5)
+
+    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (kernel_size, kernel_size))
+
+    tophat = cv.morphologyEx(blur, cv.MORPH_TOPHAT, kernel)
+
+    _, threshold = cv.threshold(tophat, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
+
+    if mask is not None:
+        threshold = cv.bitwise_and(threshold, threshold, mask=mask)
+
+    save_name = f"{file_name}_p{idx}.png" if idx is not None else f"{file_name}_p.png"
+
+    if save:
+        save_path_preprocessing = os.path.join(save_path, "Preprocessing")
+        os.makedirs(save_path_preprocessing, exist_ok=True)
+        cv.imwrite(os.path.join(save_path_preprocessing, save_name), threshold)
+    print(f"File {save_name} preprocessed.")
+
+    return threshold
+
+def preprocess_bg_isolation(
         source,
         mask,
         s = 121,
@@ -238,53 +242,3 @@ def preprocess_bg_removal(
     print(f"File {save_name} preprocessed.")
 
     return filtered
-
-def make_foreground_masks(
-        image_paths,
-        save_path = "",
-        save = False
-):
-    last_image_path = image_paths[-1] # last image used for a foreground mask
-
-    dishes, masks, coordinates, _ = detect_dishes( # dish crops from last image
-        source=last_image_path,
-        file_name=os.path.basename(last_image_path),
-        save=False
-    )
-
-    file_name = os.path.splitext(os.path.basename(last_image_path))[0]
-    foreground_masks = [preprocess(source=dish, mask=mask, file_name=file_name, kernel_size=500) for dish, mask in zip(dishes, masks)]
-
-    if save:
-        for idx, mask in enumerate(foreground_masks):
-            cv.imwrite(os.path.join(save_path, f"fg_mask{idx}"), mask)
-
-    return foreground_masks, coordinates
-
-def make_background_masks(image_paths, n_to_stack, coordinates, save, save_path):
-    first_n_image_paths = image_paths[:n_to_stack] # grabs the first n images
-
-    first_n_dishes = []
-    
-    for img_path in first_n_image_paths: # crops dishes and preprocesses them
-        dishes, masks = crop(img_path, coordinates)
-
-        preprocessed = [preprocess_bg_removal(source=dish, mask=mask, file_name=os.path.splitext(os.path.basename(img_path))[0]) for dish, mask in zip(dishes, masks)]
-        first_n_dishes.append(preprocessed)
-
-    background_masks = []
-
-    for i in range(len(first_n_dishes[0])): # combines all preprocessed images for a given dish (OR operand); results in masks of background/noise
-        group = [d[i] for d in first_n_dishes]
-        
-        stack = np.zeros_like(group[0])
-        for idx, img in enumerate(group):
-            stack = cv.bitwise_or(stack, img)
-            if save:
-                cv.imwrite(os.path.join(save_path, f"bg_mask_dish{i+1}_{idx+1}.png"), img)
-        background_masks.append(stack)
-        if save:
-            cv.imwrite(os.path.join(save_path, f"bg_mask_dish{i+1}_stack.png"), stack)
-
-    return background_masks
-
