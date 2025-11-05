aa540cb49078430469e0b5499d9b882a239977bc
jniklas03 <jakubniklas03@gmail.com>
2025-11-05 14:46:40 +0100

    cleaned up all preprocessing functions; cleaned up docs. moved fg_ and bg_masks into preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -4,47 +4,58 @@ import os
 import warnings
 
 from inputs import read_img
-from dish_detection import detect_dishes, crop
+
+def watershed():
+    pass
 
 def preprocess(
         source,
-        mask,
+        mask = None,
+        fg_mask = None,
+        bg_mask = None,
         area_filter = True,
         s = 121,
         C = 11,
         kernel_size = 3,
         min_area = 5,
         max_area = 200,
-        save=False,
+        save = False,
         save_path = "",
         file_name = "preprocessed",
         idx: int = None
         ):
     """
-    Preprocesses input image. Used in the preprocessing of just grown colonies (timelapse).
+    Preprocesses input image of cropped dish into a thresholded binary image of colonies.
+
     Returns preprocessed image.
     
     Parameters
     ----------
     source: str or np.ndarray
-        Image of dish, or string of to the image path
+        Image of dish, or string of to the image path.
     mask: np.ndarray, optional
-        Mask of background area outside of dish
+        Mask of background area outside of dish, if None the background crop won't be applied and watershedding won't work.
+    fg_mask: np.ndarray, optional
+        "Ground truth positive" mask of colonies from a time point in the future. From preprocess_fg_isolation()
+    bg_mask: np.ndarray, optional
+        "Ground truth negative" mask of the petri dish, marker, and other artifacts. From preprocess_bg_isolation().
+    area_filter: bool, default=True,
+        Filter large objects by virtue of connected components. Useful for just formed colonies to reduce the noise; turn off when colonies are larger.
     s: int, default=121
         Block size for thresholding. Bigger numbers include more to threshold.
-    C: int, defualt=11
+    C: int, default=11
         Constant to subtract from thresholding.
     kernel_size: int, optional
-        Kernel size for erosion. Used for noise removal.
+        Kernel size for erosion. Used for noise removal and colony separation.
     save: bool, default=True
-        Whether to save the cropped dishes.
+        Whether to save the preprocessed image.
     save_path: str, optional
-        Path to directory where the image is saved.
+        Path where the image should be saved.
     file_name: str, optional
         Name to save the preprocessed image as.
     idx: int, optional
-        Passed by main.py if multiple dishes processed.
-
+        Passed by a wrapper function when processing mutliple dishes.    
+    
     Returns
     -------
     np.ndarray
@@ -67,7 +78,7 @@ def preprocess(
     )
 
     if area_filter:
-        num_labels, labels, stats, centroids = cv.connectedComponentsWithStats(threshold, connectivity=8)
+        num_labels, labels, stats, _ = cv.connectedComponentsWithStats(threshold, connectivity=8)
         filtered = np.zeros_like(threshold)
 
         for i in range(1, num_labels):  # skip background
@@ -78,52 +89,63 @@ def preprocess(
         filtered = threshold
 
     kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (kernel_size, kernel_size))
+
     eroded = cv.morphologyEx(filtered, cv.MORPH_ERODE, kernel)
 
-    if mask is not None:
-        filtered = cv.bitwise_and(filtered, filtered, mask=mask)
-    else:
-        filtered = eroded
+    if not area_filter:
+        watershed()
 
-    save_name = f"{file_name}_p{idx}.png" if idx is not None else f"{file_name}_p.png"
+    if mask is not None:
+        cropped = cv.bitwise_and(eroded, eroded, mask=mask)
+    else:
+        cropped = eroded
+
+    if fg_mask and bg_mask is not None:
+            preprocessed = cv.bitwise_and(cropped, cropped, mask=fg_mask)
+            preprocessed = cv.bitwise_and(preprocessed, preprocessed, mask=cv.bitwise_not(bg_mask))
+
+    save_name = f"{file_name}_preprocessed_{idx}.png" if idx is not None else f"{file_name}_preprocessed.png"
 
     if save:
         save_path_preprocessing = os.path.join(save_path, "Preprocessing")
         os.makedirs(save_path_preprocessing, exist_ok=True)
-        cv.imwrite(os.path.join(save_path_preprocessing, save_name), filtered)
+        cv.imwrite(os.path.join(save_path_preprocessing, save_name), preprocessed)
+
     print(f"File {save_name} preprocessed.")
 
-    return filtered
+    return preprocessed
 
 def preprocess_fg_isolation(
         source,
         mask = None,
         kernel_size = 200,
-        save=False,
+        save = False,
         save_path = "",
         file_name = "preprocessed",
         idx: int = None
         ):
     """
-    Preprocesses input image. Used for standalone colony detection or for foreground masking.
+    Preprocesses input image of cropped dish into a thresholded binary image. 
+    Used on images of large colonies to be used as the positive ground truth.
+
     Returns preprocessed image.
     
     Parameters
     ----------
     source: str or np.ndarray
-        Image of dish, or string of to the image path
+        Image of dish, or string of to the image path.
     mask: np.ndarray, optional
-        Mask of background area outside of dish, passed by detect_dishes()
-    kernel_size: int, optional
-        Kernel size for "opening"; higher number yields more smoothed, generally better results, but takes longer. 
-    save: bool, default=True
-        Whether to save the cropped dishes.
+        Mask of background area outside of dish, if None the background crop won't be applied and watershedding won't work.
+    kernel_size: int, default = 200
+        Kernel size for tophat; higher number results in a smoother background and contrasted colonies, but takes longer. 
+    save: bool, default = True
+        Whether to save the preprocessed image.
     save_path: str, optional
         Path to directory where the image is saved.
     file_name: str, optional
         Name to save the preprocessed image as.
     idx: int, optional
-        Passed by main.py if multiple dishes processed.
+        Passed by a wrapper function when processing mutliple dishes.  
 
     Returns
     -------
@@ -163,7 +185,7 @@ def preprocess_bg_isolation(
         mask,
         s = 121,
         C = 11,
-        kernel_size = 5,
+        kernel_size = 3,
         min_area = 5,
         max_area = 200,
         save=False,
@@ -172,7 +194,9 @@ def preprocess_bg_isolation(
         idx: int = None
         ):
     """
-    Preprocesses input image. Basically inverse of preprocess_small. Used for removal of petri dish and other artefacts.
+    Preprocesses input image of cropped dish into a thresholded binary image. 
+    Used on images with no colonies to be used as the negative ground truth.
+
     Returns preprocessed image.
     
     Parameters
@@ -180,21 +204,21 @@ def preprocess_bg_isolation(
     source: str or np.ndarray
         Image of dish, or string of to the image path
     mask: np.ndarray, optional
-        Mask of background area outside of dish
+        Mask of background area outside of dish, if None the background crop won't be applied and watershedding won't work.
     s: int, default=121
         Block size for thresholding. Bigger numbers include more to threshold.
     C: int, defualt=11
         Constant to subtract from thresholding.
     kernel_size: int, optional
-        Kernel size for erosion. Used for noise removal.
+        Kernel size for opening. Used for closing gaps.
     save: bool, default=True
-        Whether to save the cropped dishes.
+        Whether to save the preprocessed images.
     save_path: str, optional
         Path to directory where the image is saved.
     file_name: str, optional
         Name to save the preprocessed image as.
     idx: int, optional
-        Passed by main.py if multiple dishes processed.
+        Passed by a wrapper function when processing mutliple dishes.  
 
     Returns
     -------
@@ -226,10 +250,10 @@ def preprocess_bg_isolation(
             filtered[labels == i] = 255
 
     kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (kernel_size, kernel_size))
-    opened = cv.morphologyEx(filtered, cv.MORPH_OPEN, kernel, iterations=2)
+    opened = cv.morphologyEx(filtered, cv.MORPH_OPEN, kernel)
 
     if mask is not None:
-        filtered = cv.bitwise_and(filtered, filtered, mask=mask)
+        filtered = cv.bitwise_and(opened, opened, mask=mask)
     else:
         filtered = opened
 
