a1d5bf02db8e5970131cdc5b9d5945f969b4ccf8
jniklas03 <jakubniklas03@gmail.com>
2025-11-10 16:39:36 +0100

    changes to versioning and file structure

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
-from ._version import version as __version__
-
 from .helpers import *
 from .image_manipulation import *
 from .colony_detection import *
 from .main import *
+
+__version__ = "0.1.0"
