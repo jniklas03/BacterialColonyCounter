630a63a80627158d0399ef48a32f5b8200195bf1
jniklas03 <jakubniklas03@gmail.com>
2025-11-11 15:53:28 +0100

    changed imports

--- phase/__init__.py
+++ phase/__init__.py
@@ -1,4 +1,4 @@
 from .main.main import pipeline, mult_pipeline, timelapse_pipeline
-from .image_manipulation.dish_detection import detect_dishes, crop
-from .image_manipulation.preprocessing import preprocess, preprocess_bg_isolation, preprocess_fg_isolation
+from .image_manipulation.dish_detection import detect_dishes
+from .image_manipulation.preprocessing import preprocess
 from .colony_detection.counting import detect_colonies
