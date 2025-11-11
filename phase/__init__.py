af25f661bd4d700a9bee49ad586ba1e605e99145
jniklas03 <jakubniklas03@gmail.com>
2025-11-11 14:38:17 +0100

    import changes

--- phase/__init__.py
+++ phase/__init__.py
@@ -1,1 +1,4 @@
-__version__ = "0.1.0"
+from .main.main import pipeline, mult_pipeline, timelapse_pipeline
+from .image_manipulation.dish_detection import detect_dishes, crop
+from .image_manipulation.preprocessing import preprocess, preprocess_bg_isolation, preprocess_fg_isolation
+from .colony_detection.counting import detect_colonies
